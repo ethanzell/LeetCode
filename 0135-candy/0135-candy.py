b6b4ddd64391eeefedb8cf56a7d8e91636e22187
ethanzell <100359601+ethanzell@users.noreply.github.com>
2023-10-03 08:41:20 -0400

    Time: 145 ms (49.86%), Space: 19.5 MB (14.35%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,11 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candy = [1 for k in range(n)]
+        for i in range(1,n):
+            if (ratings[i]>ratings[i-1]) and (candy[i] <= candy[i-1]):
+                candy[i] = candy[i-1] + 1
+        for i in range(n-2,-1,-1):
+            if (ratings[i]>ratings[i+1]) and (candy[i]<= candy[i+1]):
+                candy[i] = candy[i+1]+1
+        return sum(candy)
